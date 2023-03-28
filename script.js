58766abeab15cca5d09c1c9a11457767311ee790
Mizu-nee <128200501+Mizu-nee@users.noreply.github.com>
2023-03-28 17:53:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const userID = "738748102311280681";
+const statusImage = document.getElementById("status-image");
+
+async function fetchDiscordStatus() {
+  try {
+    const response = await axios.get(
+      `https://api.lanyard.rest/v1/users/${userID}`
+    );
+    const { data } = response.data;
+    const { discord_status, activities } = data;
+
+    // Lấy đường dẫn hình ảnh tương ứng với trạng thái
+    let imagePath;
+    switch (discord_status) {
+      case "online":
+        imagePath = "/public/status/online.png";
+        break;
+      case "idle":
+        imagePath = "/public/status/idle.png";
+        break;
+      case "dnd":
+        imagePath = "/public/status/dnd.png";
+        break;
+      case "offline":
+        imagePath = "/public/status/offline.png";
+        break;
+      default:
+        imagePath = "";
+        break;
+    }
+
+    // Kiểm tra trạng thái hoạt động để cập nhật đường dẫn hình ảnh
+    if (
+      activities.find(
+        (activity) => activity.type === 1 && activity.url.includes("twitch.tv")
+      )
+    ) {
+      imagePath = "/public/status/streaming.png";
+    }
+
+    // Cập nhật hình ảnh
+    statusImage.src = imagePath;
+    statusImage.alt = `Trạng thái Discord: ${discord_status}`;
+  } catch (error) {
+    console.error("Không thể lấy trạng thái Discord:", error);
+  }
+}
+
+fetchDiscordStatus();
+setInterval(fetchDiscordStatus, 1000); // Update status every 5 seconds
