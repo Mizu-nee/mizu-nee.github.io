d0daab340ae3aba24826c5062a2e10fb033c18fd
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2023-05-14 05:32:10 +0700

    up

---
+++ script.js
@@ -0,0 +1,8 @@
+// write auto redirect to https://mizu.is-a.dev website or https://miyagawamizu.github.io website
+// if you want to use this script, please change the url to your website url
+
+// redirect to https://mizu.is-a.dev
+window.location.href = "https://mizu.is-a.dev";
+
+// redirect to https://miyagawamizu.github.io
+window.location.href = "https://miyagawamizu.github.io";
