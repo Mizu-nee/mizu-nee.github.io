1f8126c07317ded17fff381e6960c55dcf1158c8
Mizu <miyagawamizu@gmail.com>
2023-05-14 05:46:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// write auto redirect to https://mizu.is-a.dev website or https://miyagawamizu.github.io website
-// if you want to use this script, please change the url to your website url
-
-// CRedirect immediately to https://mizu.is-a.dev
+// Redirect immediately to https://mizu.is-a.dev
 window.location.replace("https://mizu.is-a.dev");
-
-// redirect to https://mizu.is-a.dev
-window.location.href = "https://mizu.is-a.dev";
-
-// redirect to https://miyagawamizu.github.io
-window.location.href = "https://miyagawamizu.github.io";
