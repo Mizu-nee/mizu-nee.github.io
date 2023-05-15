1470cae7d1945744b986ae0e5ba72af205a3d9a4
Mizu <miyagawamizu@gmail.com>
2023-05-16 00:23:43 +0700

    new

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Redirect immediately to https://mizu.is-a.dev
-window.location.replace("https://mizu.is-a.dev");
+// window.location.replace("https://mizu.is-a.dev");
+setTimeout(function () {
+	window.location.href = "https://mizu.is-a.dev";
+}, 500000); // Redirect to https://mizu.is-a.dev after 5s
+
+// can't inspect element
+document.addEventListener("keydown", function (e) {
+	if (e.keyCode == 123) {
+		e.preventDefault();
+	}
+});
+
+document.addEventListener("contextmenu", function (e) {
+	e.preventDefault();
+});
